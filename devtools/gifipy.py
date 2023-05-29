ffc7b7e0f423f5de5d7ddcd2ece216f7fdbfbb8f
Akshay Ravi <58636452+AkshayraviC09YC47@users.noreply.github.com>
2023-05-30 01:12:13 +0530

    Fixed: OS Command Injection

--- devtools/gifipy.py
+++ devtools/gifipy.py
@@ -9,5 +9,5 @@ import sys
 mov_in = sys.argv[1]
 mov_out = sys.argv[2]
 
-command = f'ffmpeg -i {mov_in} -pix_fmt rgb24 -r 10 -f gif {mov_out}'
-subprocess.call(command, shell=True)
+command = ['ffmpeg', '-i', mov_in, '-pix_fmt', 'rgb24', '-r', '10', '-f', 'gif', mov_out]
+subprocess.call(command)
